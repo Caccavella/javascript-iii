f9be366810bf30b4db621212a1d966abd0a669f9
Anthony <anthony.caccavella@gmail.com>
2017-08-09 17:27:35 -0600

    Nesting practice almost completed

--- nestingPractice.js
+++ nestingPractice.js
@@ -25,26 +25,38 @@ var employees = [{
   "email": "lhandsheart4@fotki.com",
   "department": "Research and Development"
 }]
-
-// Create a function called 'employeeUpdater'. employeeUpdater will loop over the array above and perform the following: 
+// Create a function called 'employeeUpdater'. employeeUpdater will loop over the array above and perform the following:
 //  1. If employee's first name is Theo, remove that employee because he just got fired.
 //  2. If the employee's first name is Lorie, change her department to 'HR'.
-//  3. Return the updated employee array. 
+//  3. Return the updated employee array.
+
+
+
+
 
 // Code here
 
 
-
-
-
-
-// The array below represents IDs tied to reported workplace accidents. An employee accidentally entered in duplicates to array, making it look as though there are more accidents this year than there actually are. 
+// The array below represents IDs tied to reported workplace accidents. An employee accidentally entered in duplicates to array, making 
+//it look as though there are more accidents this year than there actually are. 
 // 1. Write a function called 'removeDuplicates' that will remove all duplicate entries in the workplaceAccidents array.
-// 2. Use nested for loops to check for duplicate numbers, and then remove the duplicates from the array. Loop over the array from front to back.
+// 2. Use nested for loops to check for duplicate numbers, and then remove the duplicates from the array. Loop over the array 
+//from front to back.
 // 3. Return the updated array.
+  var newarr = (function removeDuplicates(workplaceAccidents){
+  var m = {}, newarr = []
+  for (var i=0; i < workplaceAccidents.length; i++) {
+    var v = workplaceAccidents[i];
+    if (!m[v]) {
+      newarr.push(v);
+      m[v]=true;
+    }
+  }
+  return newarr;
+})(arr);
+
 
 var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
-
 
 // Code here
 
@@ -53,27 +65,18 @@ var cat = {
   name: 'Fluffy',
   catFriends: [{
     name: 'Grumpy',
-    actions: {
-      feed: function() {
-        alert('Grumpy is no longer grumpy.')
-      },
-      petBelly: function() {
-        alert('Grumpy is now angry.')
-      }
-    }
+    activities: ['be grumpy', 'eat food']
   }, {
     name: 'Lazy Bones',
-    actions: {
-      playTime: function() {
-        alert('Lazy Bones watches you play with laser pointer')
-      }
-    }
-  }]
+    activities: ['sleep', 'pre-sleep naps']
+    }]
 }
-
-// You need to feed on of Fluffy's friends, Grumpy. Navigate the nested arrays and objects to:
-// 1. invoke the 'feed' method.
+// Fluffy has two friends, Grumpy and Lazy Bones. 
 
 
 
+// 1. Assign the value of Grumpy's 2nd activity to the variable below.
 
+
+// Code here
+var grumpyActivity {activities} = catFriends.activities;
